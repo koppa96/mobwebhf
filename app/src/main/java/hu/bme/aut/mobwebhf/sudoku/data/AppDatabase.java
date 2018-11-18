4614f392c9d6063d65010467cb5b699c36fd5304
koppa96 <koppa96@sch.bme.hu>
2018-11-18 03:20:51 +0100

    Added necessary migrations

--- app/src/main/java/hu/bme/aut/mobwebhf/sudoku/data/AppDatabase.java
+++ app/src/main/java/hu/bme/aut/mobwebhf/sudoku/data/AppDatabase.java
@@ -5,6 +5,7 @@ import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
@@ -16,23 +17,39 @@ import java.util.Scanner;
 import hu.bme.aut.mobwebhf.sudoku.R;
 
 @Database(
-        entities = { Sudoku.class, Highscore.class },
-        version = 1
+        entities = { Sudoku.class, Highscore.class, SavedSudoku.class },
+        version = 3
 )
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
     public abstract SudokuDao sudokuDao();
     public abstract HighscoreDao highscoreDao();
+    public abstract SavedSudokuDao savedSudokuDao();
 
     public static AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context, AppDatabase.class, "sudoku-db")
                            .addCallback(new AppDatabaseCallback(context))
+                           .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
                            .build();
         }
 
         return instance;
     }
+
+    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS 'savedsudokus' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'board' TEXT)");
+        }
+    };
+
+    private static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE 'savedsudokus' ADD COLUMN 'time' INTEGER");
+        }
+    };
 
     private static class AppDatabaseCallback extends RoomDatabase.Callback {
         private Context context;
